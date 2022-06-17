5a184f548e1c17f116910b9aeeaed4d89f6842c8
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-18 01:16:37 +0300

    Implemented add survey function for admin

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Survey;
+use App\Models\Question;
+use App\Models\Option;
 
 class AdminController extends Controller
 {
-    //
+    public function addSurvey(Request $request){
+        $survey = new Survey;
+        $survey->title = $request->title;
+        $survey->save();
+
+        $question_count=$request->question_count;
+
+        for ($x = 0; $x <=$question_count ; $x++) {
+
+        $question=new Question;
+        $question->text=$request->question;
+        $question->survey_id=$survey->id;
+        $question->question_type = $request->question_type;
+        $question->save();
+
+        }
+
+         $option_count=$request->option_count;
+
+        for ($x = 0; $x <=$option_count ; $x++) {
+
+            $options=$request->options;
+            $option_arr=explode(',',$options);
+            $myoption=new Option;
+            $myoption->value=$option_arr[$x];
+            $myoption->question_id=$question->id;
+            $myoption->save();
+        }
+
+        return response()->json([
+            "status" => "Success",
+            "survey" => $survey,
+            "question" => $question,
+            "options"=> $myoption
+        ], 200);
+    }
 }
